b03ea0a988169cba1ff48ed470e5a7f153fed654
Idrek <iddarg@gmail.com>
2022-05-17 10:36:12 +0200

    Add `BuildApp` target to `build.fsx` file

--- build.fsx
+++ build.fsx
@@ -4,6 +4,7 @@ nuget Fake.IO.FileSystem
 nuget Fake.Core.Target //"
 
 open Fake.Core
+open Fake.DotNet
 open Fake.IO
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
@@ -17,8 +18,15 @@ Target.create "Clean" (fun _ ->
     ++ "test/**/obj"
     |> Shell.cleanDirs)
 
+Target.create "BuildApp" (fun _ ->
+    !! "src/**/*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
 Target.create "All" ignore
 
-"Clean" ==> "All"
+"Clean" 
+    ==> "BuildApp" 
+    ==> "All"
 
 Target.runOrDefault "All"
